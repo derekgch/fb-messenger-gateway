df7951b7d07c008420ebe40fb130715ee5896a16
[Derek] <derek@derekgc.com>
2019-04-17 17:23:11 -0400

    changed back to 1337

--- quick-start/app.js
+++ quick-start/app.js
@@ -30,7 +30,7 @@ const
   app = express().use(body_parser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 443, () => {
+app.listen(process.env.PORT || 1337, () => {
   console.log('webhook is listening');
   console.log('PAGE_ACCESS_TOKEN', PAGE_ACCESS_TOKEN);
 
